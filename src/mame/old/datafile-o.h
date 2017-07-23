8068c85f029f0ee77a19115fa1a5a860b3f92bd4
Robbbert <Robbbert@users.noreply.github.com>
2017-07-23 14:58:07 +1000

    *clean

---
+++ src/mame/old/datafile-o.h
@@ -0,0 +1,9 @@
+// license:BSD-3-Clause
+// copyright-holders:Chris Kirmse, Mike Haaland, René Single, Mamesick
+
+#ifndef DATAFILE_H
+#define DATAFILE_H
+
+char * GetGameHistory(int driver_index);
+
+#endif
