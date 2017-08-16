a8ab00ee7eba297e33aa73f31e68ff84bf392399
Robbbert <Robbbert@users.noreply.github.com>
2017-08-16 20:00:22 +1000

    *Fixed the build.

---
+++ docs/release/build/make.bat
@@ -0,0 +1,5 @@
+del arcade32.sym
+:start
+del arcade32.exe
+if exist arcade32.exe goto start
+make32 -j4 %1 %2 %3
---
+++ docs/release/build/release.bat
@@ -0,0 +1,30 @@
+@echo off
+echo HAVE YOU UPDATED the version number in WINUI.H ???
+pause
+
+
+call newsrc.bat
+call clean.bat
+call clean.bat
+call clean.bat
+call clean.bat
+
+rem --- 32bit ---
+del arcade32.exe
+del arcade32.sym
+call make32 -j4 %1 %2 %3
+if not exist arcade32.exe goto end
+
+rem --- 64bit ---
+del arcade64.exe
+del arcade64.sym
+call make64 -j4 %1 %2 %3
+if not exist arcade64.exe goto end
+
+cls
+echo Compile was successful.
+echo.
+echo 7Z up each exe;
+echo and RAR the source.
+
+:end
