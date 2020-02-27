b05559773c12f5eb0ea08fb5eb6ab8de3a075307
Robbbert <Robbbert@users.noreply.github.com>
2020-02-27 11:03:39 +1100

    0.219 Release files

--- src/osd/winui/winui.h
+++ src/osd/winui/winui.h
@@ -84,7 +84,7 @@
 #endif
 #define MAMENAME			"MAME"
 
-#define MAME_VERSION		"0.218"
+#define MAME_VERSION		"0.219"
 #define MAMEUIFX_VERSION	MAME_VERSION " (" __DATE__")"
 
 #define SEARCH_PROMPT		"<search here>"
