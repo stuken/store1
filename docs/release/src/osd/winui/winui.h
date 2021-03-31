aa81f6ea1860183acda0309752a928b0b2d771ee
Robbbert <Robbbert@users.noreply.github.com>
2021-03-31 15:48:51 +1100

    0.230 Release files

--- docs/release/src/osd/winui/winui.h
+++ docs/release/src/osd/winui/winui.h
@@ -85,7 +85,7 @@
 #endif
 #define MAMENAME			"MAME"
 
-#define MAME_VERSION		"0.229"
+#define MAME_VERSION		"0.230"
 #define MAMEUIFX_VERSION	MAME_VERSION " (" __DATE__")"
 
 #define SEARCH_PROMPT		"<search here>"
